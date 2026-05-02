6e1730d74a6e854aff5e34ed3c6e09431960d2ae
H2RkawaNinja <deine-email@beispiel.com>
2026-05-02 03:49:52 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -28,8 +28,8 @@ app.use(cors({
     : 'http://localhost:5173',
   credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
